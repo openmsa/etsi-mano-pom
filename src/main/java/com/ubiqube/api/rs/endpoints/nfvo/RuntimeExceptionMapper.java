e6506779bb36acfe566784913eb9d1eda067bc7d
Vignaud Olivier <ovi@ubiqube.com>
2019-05-31 14:50:19 +0200

    Added: Global Exception MApper.

---
+++ src/main/java/com/ubiqube/api/rs/endpoints/nfvo/RuntimeExceptionMapper.java
@@ -0,0 +1,28 @@
+package com.ubiqube.api.rs.endpoints.nfvo;
+
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.ext.ExceptionMapper;
+import javax.ws.rs.ext.Provider;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.ubiqube.api.ejb.nfvo.vnf.ProblemDetails;
+
+@Provider
+public class RuntimeExceptionMapper implements ExceptionMapper<Throwable> {
+	private static final Logger LOG = LoggerFactory.getLogger(RuntimeExceptionMapper.class);
+
+	@Override
+	public Response toResponse(Throwable _exception) {
+		LOG.error("An error has occured.", _exception);
+		final int statusCode = 501;
+		final String message = _exception.getMessage();
+		return Response.status(statusCode)
+				.entity(new ProblemDetails(statusCode, message))
+				.type(MediaType.APPLICATION_JSON_TYPE)
+				.build();
+	}
+
+}
