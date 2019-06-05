af29b35b783b4565c891bbfceadfe1ca039e12ed
Vignaud Olivier <ovi@ubiqube.com>
2019-06-05 17:06:11 +0200

    Fixed: Serialization of subscription Methods.

--- src/main/java/com/ubiqube/api/rs/endpoints/nfvo/vnf/DefaultApiServiceImpl.java
+++ src/main/java/com/ubiqube/api/rs/endpoints/nfvo/vnf/DefaultApiServiceImpl.java
@@ -341,9 +341,8 @@ public class DefaultApiServiceImpl implements DefaultApi {
 			@ApiResponse(code = 406, message = "If the \"Accept\" header does not contain at least one name of a content type for which the NFVO can provide a representation of the VNFD, the NFVO shall respond with this response code.         ", response = ProblemDetails.class), @ApiResponse(code = 416, message = "Requested Range Not Satisfiable The byte range passed in the \"Range\" header did not match any available byte range in the VNF package file (e.g. \"access after end of file\"). The response body may contain a ProblemDetails structure. ", response = ProblemDetails.class), @ApiResponse(code = 500, message = "Internal Server Error If there is an application error not related to the client's input that cannot be easily mapped to any other HTTP response code (\"catch all error\"), the API producer shall respond withthis response code. The ProblemDetails structure shall be provided, and shall include in the \"detail\" attribute more information about the source of the problem. ", response = ProblemDetails.class),
 			@ApiResponse(code = 503, message = "Service Unavailable If the API producer encounters an internal overload situation of itself or of a system it relies on, it should respond with this response code, following the provisions in IETF RFC 7231 [13] for the use of the Retry-After HTTP header and for the alternative to refuse the connection. The \"ProblemDetails\" structure may be omitted. ", response = ProblemDetails.class) })
 	public String subscriptionsSubscriptionIdGet(@PathParam("subscriptionId") String subscriptionId, @HeaderParam("Accept") String accept, @Context SecurityContext securityContext) {
-		final InlineResponse2001 inlineResponse2001 = new InlineResponse2001();
-		inlineResponse2001.setPkgmSubscription(subscriptionRepository.get(subscriptionId).getSubscriptionsPkgmSubscription());
-		return object2String(inlineResponse2001);
+		final SubscriptionsPkgmSubscription subs = subscriptionRepository.get(subscriptionId).getSubscriptionsPkgmSubscription();
+		return object2String(subs);
 
 	}
 
@@ -609,7 +608,10 @@ public class DefaultApiServiceImpl implements DefaultApi {
 			@ApiResponse(code = 500, message = "Internal Server Error If there is an application error not related to the client's input that cannot be easily mapped to any other HTTP response code (\"catch all error\"), the API producer shall respond withthis response code. The ProblemDetails structure shall be provided, and shall include in the \"detail\" attribute more information about the source of the problem. ", response = ProblemDetails.class), @ApiResponse(code = 503, message = "Service Unavailable If the API producer encounters an internal overload situation of itself or of a system it relies on, it should respond with this response code, following the provisions in IETF RFC 7231 [13] for the use of the Retry-After HTTP header and for the alternative to refuse the connection. The \"ProblemDetails\" structure may be omitted. ", response = ProblemDetails.class) })
 	public Response vnfPackagesVnfPkgIdDelete(@PathParam("vnfPkgId") String vnfPkgId, @Context SecurityContext securityContext) {
 		final String uri = new StringBuilder().append(REPOSITORY_NVFO_DATAFILE_BASE_PATH).append("/").append(sanitize(vnfPkgId)).toString();
-		getVnfPkgIndividualInfoOrCheckOnboardingStatus(vnfPkgId, true);
+		final VnfPkgInfo vnfPkgInfo = vnfPackageRepository.get(vnfPkgId);
+		if (!"DISABLED".equals(vnfPkgInfo.getOperationalState())) {
+			throw new ConflictException("Packaged is enabled.");
+		}
 		boolean isVnfdMetafile = false;
 		try {
 			isVnfdMetafile = repositoryService.exists(uri);
@@ -944,7 +946,7 @@ public class DefaultApiServiceImpl implements DefaultApi {
 			if (MediaType.TEXT_PLAIN.equals(accept)) {
 				final RepositoryElement repositoryElement = repositoryService.getElement(uri);
 				final String content = new String(repositoryService.getRepositoryElementContent(repositoryElement));
-				return Response.ok(JSONObject.fromObject(content), MediaType.APPLICATION_JSON).build();
+				return Response.ok(content, MediaType.APPLICATION_JSON).build();
 			} else if ("application/zip".equals(accept)
 					|| ("application/zip".equals(accept) && MediaType.TEXT_PLAIN.equals(accept))) {
 				return getZipArchive(null, listvnfPckgFiles);
