d5fe8d119c9875b04ea5acdbcfee37d73bcf1d41
Vignaud Olivier <ovi@ubiqube.com>
2019-06-12 15:46:46 +0200

    Changed: Better handling of SOL005 API

--- src/main/java/com/ubiqube/api/rs/endpoints/nfvo/vnf/VnfPkgSol003.java
+++ src/main/java/com/ubiqube/api/rs/endpoints/nfvo/vnf/VnfPkgSol003.java
@@ -84,6 +84,7 @@ import net.sf.json.JSONObject;
 @Api(value = "/sol003/vnfpkgm/v1", description = "")
 public class VnfPkgSol003 extends BaseApi implements DefaultSol003Api {
 	private static final Logger LOGGER = LoggerFactory.getLogger(VnfPkgSol003.class);
+	private final VnfManagement vnfManagement = new VnfManagement();
 
 	/**
 	 * Map YAML file to JsonObject.
@@ -447,11 +448,7 @@ public class VnfPkgSol003 extends BaseApi implements DefaultSol003Api {
 			@ApiResponse(code = 406, message = "If the \"Accept\" header does not contain at least one name of a content type for which the NFVO can provide a representation of the VNFD, the NFVO shall respond with this response code.         ", response = ProblemDetails.class), @ApiResponse(code = 416, message = "Requested Range Not Satisfiable The byte range passed in the \"Range\" header did not match any available byte range in the VNF package file (e.g. \"access after end of file\"). The response body may contain a ProblemDetails structure. ", response = ProblemDetails.class), @ApiResponse(code = 500, message = "Internal Server Error If there is an application error not related to the client's input that cannot be easily mapped to any other HTTP response code (\"catch all error\"), the API producer shall respond withthis response code. The ProblemDetails structure shall be provided, and shall include in the \"detail\" attribute more information about the source of the problem. ", response = ProblemDetails.class),
 			@ApiResponse(code = 503, message = "Service Unavailable If the API producer encounters an internal overload situation of itself or of a system it relies on, it should respond with this response code, following the provisions in IETF RFC 7231 [13] for the use of the Retry-After HTTP header and for the alternative to refuse the connection. The \"ProblemDetails\" structure may be omitted. ", response = ProblemDetails.class) })
 	public Response vnfPackagesVnfPkgIdGet(@PathParam("vnfPkgId") String vnfPkgId, @HeaderParam("Accept") String accept) throws ServiceException {
-
-		LOGGER.info(new StringBuilder("Method called: ").append(new Object() {
-		}.getClass().getEnclosingMethod().getName()).toString());
-
-		final VnfPkgInfo vnfPkgInfo = getVnfPkgIndividualInfoOrCheckOnboardingStatus(vnfPkgId, false);
+		final VnfPkgInfo vnfPkgInfo = vnfManagement.vnfPackagesVnfPkgIdGet(vnfPkgId, accept);
 		final VnfPackagesVnfPkgIdGetResponse vnfPackagesVnfPkgIdGetResponse = new VnfPackagesVnfPkgIdGetResponse();
 		vnfPackagesVnfPkgIdGetResponse.setVnfPkgInfo(vnfPkgInfo);
 		return Response.ok(vnfPkgInfo).build();
