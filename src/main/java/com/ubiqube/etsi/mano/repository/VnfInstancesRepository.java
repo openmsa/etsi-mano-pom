55960aba0f323224e9976d97f5dd32c9a19a9ac5
Olivier Vignaud <ovi@ubiqube.com>
2019-07-12 09:50:59 +0200

    Added: query method in VnfInstance repository.

--- src/main/java/com/ubiqube/etsi/mano/repository/VnfInstancesRepository.java
+++ src/main/java/com/ubiqube/etsi/mano/repository/VnfInstancesRepository.java
@@ -1,17 +1,25 @@
 package com.ubiqube.etsi.mano.repository;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.UUID;
 
 import javax.inject.Inject;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Repository;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.ubiqube.api.entities.repository.RepositoryElement;
+import com.ubiqube.api.exception.ServiceException;
 import com.ubiqube.api.interfaces.repository.RepositoryService;
+import com.ubiqube.etsi.mano.exception.GenericException;
 import com.ubiqube.etsi.mano.model.nslcm.sol003.VnfInstance;
 
 @Repository
 public class VnfInstancesRepository extends AbstractGenericRepository<VnfInstance> {
+	private static final Logger LOG = LoggerFactory.getLogger(VnfInstancesRepository.class);
 	private static final String REPOSITORY_VNF_INSTANCE_DATAFILE_BASE_PATH = "Datafiles/NFVO/vnf_instances";
 
 	@Inject
@@ -19,7 +27,7 @@ public class VnfInstancesRepository extends AbstractGenericRepository<VnfInstanc
 		super(_mapper, _repositoryService);
 	}
 
-	@Override 
+	@Override
 	String getUriForId(String _id) {
 		return REPOSITORY_VNF_INSTANCE_DATAFILE_BASE_PATH + "/" + _id + "/vnfInstance.json";
 	}
@@ -39,4 +47,27 @@ public class VnfInstancesRepository extends AbstractGenericRepository<VnfInstanc
 		return VnfInstance.class;
 	}
 
+	public List<VnfInstance> query() {
+		final List<VnfInstance> ret = new ArrayList<>();
+		List<String> listFilesInFolder;
+		try {
+			listFilesInFolder = repositoryService.doSearch(REPOSITORY_VNF_INSTANCE_DATAFILE_BASE_PATH, "vnfInstance.json");
+		} catch (final ServiceException e) {
+			throw new GenericException(e);
+		}
+		for (final String string : listFilesInFolder) {
+			LOG.debug("Loading: {}", string);
+			final RepositoryElement repositoryElement = repositoryService.getElement(string);
+			final byte[] repositoryContent = repositoryService.getRepositoryElementContent(repositoryElement);
+			final String content = new String(repositoryContent);
+			try {
+				final VnfInstance vnfinstance = (VnfInstance) mapper.readValue(content, getClazz());
+				ret.add(vnfinstance);
+
+			} catch (final Exception e) {
+				throw new GenericException(e);
+			}
+		}
+		return ret;
+	}
 }
