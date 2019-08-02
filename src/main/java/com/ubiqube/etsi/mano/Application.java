36c2ab2f7383745fa9df7d7c3c8d02c065d82dc6
Olivier Vignaud <ovi@ubiqube.com>
2019-08-02 15:51:08 +0200

    removed a Useless bean.

--- src/main/java/com/ubiqube/etsi/mano/Application.java
+++ src/main/java/com/ubiqube/etsi/mano/Application.java
@@ -6,10 +6,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
-import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
-import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonInclude.Include;
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -27,7 +25,7 @@ public class Application extends SpringBootServletInitializer {
 	@Bean
 	public ObjectMapper objectMapper() {
 		final ObjectMapper objectMapper = new ObjectMapper();
-		LOG.info("Initializing JSON Object mapper.");
+		LOG.info("Initializing JSON Object mapper: {}", objectMapper);
 		objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
 		objectMapper.configure(SerializationFeature.INDENT_OUTPUT, true);
 		objectMapper.setSerializationInclusion(Include.NON_NULL);
@@ -35,11 +33,4 @@ public class Application extends SpringBootServletInitializer {
 		return objectMapper;
 	}
 
-	@Bean
-	public Jackson2ObjectMapperBuilder objectMapperBuilder() {
-		final Jackson2ObjectMapperBuilder builder = new Jackson2ObjectMapperBuilder();
-		builder.serializationInclusion(JsonInclude.Include.NON_NULL);
-		builder.indentOutput(true);
-		return builder;
-	}
 }
