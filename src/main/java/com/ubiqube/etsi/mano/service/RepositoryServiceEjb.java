8613775d2bd6949fd8f0ba2dfcb97fbfba448b03
Olivier Vignaud <ovi@ubiqube.com>
2019-09-02 11:38:59 +0200

    Added: LCM OP OCCS repository.

--- src/main/java/com/ubiqube/etsi/mano/service/RepositoryServiceEjb.java
+++ src/main/java/com/ubiqube/etsi/mano/service/RepositoryServiceEjb.java
@@ -40,13 +40,14 @@ public class RepositoryServiceEjb implements RepositoryService {
 	protected static final String REPOSITORY_NSD_BASE_PATH = NVFO_DATAFILE_BASE_PATH + "/nsd";
 	protected static final String REPOSITORY_VNF_INSTANCE_DATAFILE_BASE_PATH = NVFO_DATAFILE_BASE_PATH + "/vnf_instances";
 	protected static final String REPOSITORY_NS_INSTANCE_DATAFILE_BASE_PATH = NVFO_DATAFILE_BASE_PATH + "/ns_instances";
+	protected static final String REPOSITORY_LCM_OP_OCCS_DATAFILE_BASE_PATH = NVFO_DATAFILE_BASE_PATH + "/lcm-op-occs";
 	/** EJB Instance. */
 	private RepositoryService repositoryService;
 
 	/**
 	 * Constructor.
 	 */
-	public RepositoryServiceEjb(JndiWrapper _jndiWrapper) {
+	public RepositoryServiceEjb(final JndiWrapper _jndiWrapper) {
 		try {
 			repositoryService = (RepositoryService) _jndiWrapper.lookup("ubi-jentreprise/RepositoryManagerBean/remote-com.ubiqube.api.interfaces.repository.RepositoryService");
 			init();
@@ -93,85 +94,88 @@ public class RepositoryServiceEjb implements RepositoryService {
 		if (!repositoryService.exists(REPOSITORY_NS_INSTANCE_DATAFILE_BASE_PATH)) {
 			repositoryService.addDirectory(REPOSITORY_NS_INSTANCE_DATAFILE_BASE_PATH, "", MANO, NCROOT);
 		}
+		if (!repositoryService.exists(REPOSITORY_LCM_OP_OCCS_DATAFILE_BASE_PATH)) {
+			repositoryService.addDirectory(REPOSITORY_LCM_OP_OCCS_DATAFILE_BASE_PATH, "", MANO, NCROOT);
+		}
 	}
 
 	@Override
-	public void addCustomer(String _arg0, String _arg1) throws ServiceException {
+	public void addCustomer(final String _arg0, final String _arg1) throws ServiceException {
 		repositoryService.addCustomer(_arg0, _arg1);
 	}
 
 	@Override
-	public void addDirectory(String _arg0, String _arg1, String _arg2, String _arg3) throws ServiceException {
+	public void addDirectory(final String _arg0, final String _arg1, final String _arg2, final String _arg3) throws ServiceException {
 		repositoryService.addDirectory(_arg0, _arg1, _arg2, _arg3);
 	}
 
 	@Override
-	public void addFile(String _arg0, String _arg1, String _arg2, byte[] _arg3, String _arg4) throws ServiceException {
+	public void addFile(final String _arg0, final String _arg1, final String _arg2, final byte[] _arg3, final String _arg4) throws ServiceException {
 		repositoryService.addFile(_arg0, _arg1, _arg2, _arg3, _arg4);
 	}
 
 	@Override
-	public void addFile(String _arg0, String _arg1, String _arg2, String _arg3, String _arg4) throws ServiceException {
+	public void addFile(final String _arg0, final String _arg1, final String _arg2, final String _arg3, final String _arg4) throws ServiceException {
 		repositoryService.addFile(_arg0, _arg1, _arg2, _arg3, _arg4);
 	}
 
 	@Override
-	public void addLargeFileContent(String _arg0, String _arg1, String _arg2, byte[] _arg3, String _arg4) throws ServiceException {
+	public void addLargeFileContent(final String _arg0, final String _arg1, final String _arg2, final byte[] _arg3, final String _arg4) throws ServiceException {
 		repositoryService.addLargeFileContent(_arg0, _arg1, _arg2, _arg3, _arg4);
 	}
 
 	@Override
-	public void addManufacturer(String _arg0, String _arg1) throws ServiceException {
+	public void addManufacturer(final String _arg0, final String _arg1) throws ServiceException {
 		repositoryService.addManufacturer(_arg0, _arg1);
 	}
 
 	@Override
-	public void addModel(String _arg0, String _arg1) throws ServiceException {
+	public void addModel(final String _arg0, final String _arg1) throws ServiceException {
 		repositoryService.addModel(_arg0, _arg1);
 	}
 
 	@Override
-	public void addOperator(String _arg0, String _arg1) throws ServiceException {
+	public void addOperator(final String _arg0, final String _arg1) throws ServiceException {
 		repositoryService.addOperator(_arg0, _arg1);
 	}
 
 	@Override
-	public boolean addRepositoryElementContent(RepositoryElement _arg0, byte[] _arg1, String _arg2) throws RepositoryException {
+	public boolean addRepositoryElementContent(final RepositoryElement _arg0, final byte[] _arg1, final String _arg2) throws RepositoryException {
 		return repositoryService.addRepositoryElementContent(_arg0, _arg1, _arg2);
 	}
 
 	@Override
-	public void addRepositoryElementContent(RepositoryElementUpload _arg0, String _arg1, String _arg2) throws RepositoryException {
+	public void addRepositoryElementContent(final RepositoryElementUpload _arg0, final String _arg1, final String _arg2) throws RepositoryException {
 		repositoryService.addRepositoryElementContent(_arg0, _arg1, _arg2);
 	}
 
 	@Override
-	public void copyRepositoryDirContent(RepositoryElement _arg0, RepositoryElement _arg1, String _arg2) throws RepositoryException {
+	public void copyRepositoryDirContent(final RepositoryElement _arg0, final RepositoryElement _arg1, final String _arg2) throws RepositoryException {
 		repositoryService.copyRepositoryDirContent(_arg0, _arg1, _arg2);
 	}
 
 	@Override
-	public void copyRepositoryElementContent(RepositoryElement _arg0, String _arg1, String _arg2) throws RepositoryException {
+	public void copyRepositoryElementContent(final RepositoryElement _arg0, final String _arg1, final String _arg2) throws RepositoryException {
 		repositoryService.copyRepositoryElementContent(_arg0, _arg1, _arg2);
 	}
 
 	@Override
-	public void deleteRepositoryElement(RepositoryElement _arg0, String _arg1) throws RepositoryException {
+	public void deleteRepositoryElement(final RepositoryElement _arg0, final String _arg1) throws RepositoryException {
 		repositoryService.deleteRepositoryElement(_arg0, _arg1);
 	}
 
 	@Override
-	public List<String> doSearch(String _arg0, String _arg1) throws ServiceException {
+	public List<String> doSearch(final String _arg0, final String _arg1) throws ServiceException {
 		return repositoryService.doSearch(_arg0, _arg1);
 	}
 
 	@Override
-	public boolean exists(String _arg0) throws ServiceException {
+	public boolean exists(final String _arg0) throws ServiceException {
 		return repositoryService.exists(_arg0);
 	}
 
 	@Override
-	public String getAbsolutePath(String _arg0) throws RepositoryException {
+	public String getAbsolutePath(final String _arg0) throws RepositoryException {
 		return repositoryService.getAbsolutePath(_arg0);
 	}
 
@@ -181,17 +185,17 @@ public class RepositoryServiceEjb implements RepositoryService {
 	}
 
 	@Override
-	public List<RepositoryElement> getChildren(String _arg0, boolean _arg1, String _arg2) throws RepositoryException {
+	public List<RepositoryElement> getChildren(final String _arg0, final boolean _arg1, final String _arg2) throws RepositoryException {
 		return repositoryService.getChildren(_arg0, _arg1, _arg2);
 	}
 
 	@Override
-	public List<RepositoryElement> getChildren(String _arg0, boolean _arg1) throws RepositoryException {
+	public List<RepositoryElement> getChildren(final String _arg0, final boolean _arg1) throws RepositoryException {
 		return repositoryService.getChildren(_arg0, _arg1);
 	}
 
 	@Override
-	public List<RepositoryElementType> getChildrenType(Actor _arg0, RepositoryElement _arg1) throws RepositoryException {
+	public List<RepositoryElementType> getChildrenType(final Actor _arg0, final RepositoryElement _arg1) throws RepositoryException {
 		return repositoryService.getChildrenType(_arg0, _arg1);
 	}
 
@@ -201,152 +205,152 @@ public class RepositoryServiceEjb implements RepositoryService {
 	}
 
 	@Override
-	public RepositoryElement getElement(String _arg0) throws RepositoryException {
+	public RepositoryElement getElement(final String _arg0) throws RepositoryException {
 		return repositoryService.getElement(_arg0);
 	}
 
 	@Override
-	public String getElementAbsoluteURI(RepositoryElement _arg0) throws RepositoryException {
+	public String getElementAbsoluteURI(final RepositoryElement _arg0) throws RepositoryException {
 		return repositoryService.getElementAbsoluteURI(_arg0);
 	}
 
 	@Override
-	public String getNextUploadName(RepositoryElement _arg0, String _arg1, String _arg2) throws RepositoryException {
+	public String getNextUploadName(final RepositoryElement _arg0, final String _arg1, final String _arg2) throws RepositoryException {
 		return repositoryService.getNextUploadName(_arg0, _arg1, _arg2);
 	}
 
 	@Override
-	public byte[] getRepositoryElementContent(RepositoryElement _arg0) throws RepositoryException {
+	public byte[] getRepositoryElementContent(final RepositoryElement _arg0) throws RepositoryException {
 		return repositoryService.getRepositoryElementContent(_arg0);
 	}
 
 	@Override
-	public String[] getRepositoryKeysByCustomization(String _arg0) throws ServiceException {
+	public String[] getRepositoryKeysByCustomization(final String _arg0) throws ServiceException {
 		return repositoryService.getRepositoryKeysByCustomization(_arg0);
 	}
 
 	@Override
-	public String getRepositoryName(RepositoryElement _arg0) throws RepositoryException {
+	public String getRepositoryName(final RepositoryElement _arg0) throws RepositoryException {
 		return repositoryService.getRepositoryName(_arg0);
 	}
 
 	@Override
-	public byte[] getSVNFileContent(String _arg0) throws RepositoryException {
+	public byte[] getSVNFileContent(final String _arg0) throws RepositoryException {
 		return repositoryService.getSVNFileContent(_arg0);
 	}
 
 	@Override
-	public List<String> getUnallowedEntityList(long _arg0, String _arg1) throws ConfigurationException {
+	public List<String> getUnallowedEntityList(final long _arg0, final String _arg1) throws ConfigurationException {
 		return repositoryService.getUnallowedEntityList(_arg0, _arg1);
 	}
 
 	@Override
-	public String getUploadURIFromFileURI(String _arg0) {
+	public String getUploadURIFromFileURI(final String _arg0) {
 		return repositoryService.getUploadURIFromFileURI(_arg0);
 	}
 
 	@Override
-	public List<String> getUsage(long _arg0, String _arg1) throws ConfigurationException {
+	public List<String> getUsage(final long _arg0, final String _arg1) throws ConfigurationException {
 		return repositoryService.getUsage(_arg0, _arg1);
 	}
 
 	@Override
-	public boolean hasModificationRights(Actor _arg0, String _arg1) throws ConfigurationException {
+	public boolean hasModificationRights(final Actor _arg0, final String _arg1) throws ConfigurationException {
 		return repositoryService.hasModificationRights(_arg0, _arg1);
 	}
 
 	@Override
-	public boolean isBinary(String _arg0) throws RepositoryException {
+	public boolean isBinary(final String _arg0) throws RepositoryException {
 		return repositoryService.isBinary(_arg0);
 	}
 
 	@Override
-	public boolean isReadOnly(RepositoryElement _arg0) throws RepositoryException {
+	public boolean isReadOnly(final RepositoryElement _arg0) throws RepositoryException {
 		return repositoryService.isReadOnly(_arg0);
 	}
 
 	@Override
-	public boolean modifyRepositoryElementContent(ManagerId _arg0, RepositoryElement _arg1, byte[] _arg2, String _arg3) throws RepositoryException {
+	public boolean modifyRepositoryElementContent(final ManagerId _arg0, final RepositoryElement _arg1, final byte[] _arg2, final String _arg3) throws RepositoryException {
 		return repositoryService.modifyRepositoryElementContent(_arg0, _arg1, _arg2, _arg3);
 	}
 
 	@Override
-	public void modifyRepositoryElementContent(ManagerId _arg0, RepositoryElementUpload _arg1, String _arg2, String _arg3) throws RepositoryException {
+	public void modifyRepositoryElementContent(final ManagerId _arg0, final RepositoryElementUpload _arg1, final String _arg2, final String _arg3) throws RepositoryException {
 		repositoryService.modifyRepositoryElementContent(_arg0, _arg1, _arg2, _arg3);
 	}
 
 	@Override
-	public Properties readPropertiesFile(String _arg0) throws ServiceException, FileNotFoundException, IOException {
+	public Properties readPropertiesFile(final String _arg0) throws ServiceException, FileNotFoundException, IOException {
 		return repositoryService.readPropertiesFile(_arg0);
 	}
 
 	@Override
-	public void removeCustomer(String _arg0, String _arg1) throws ServiceException {
+	public void removeCustomer(final String _arg0, final String _arg1) throws ServiceException {
 		repositoryService.removeCustomer(_arg0, _arg1);
 	}
 
 	@Override
-	public void removeDirectory(String _arg0, String _arg1) throws ServiceException {
+	public void removeDirectory(final String _arg0, final String _arg1) throws ServiceException {
 		repositoryService.removeDirectory(_arg0, _arg1);
 	}
 
 	@Override
-	public void removeFile(String _arg0, String _arg1) throws ServiceException {
+	public void removeFile(final String _arg0, final String _arg1) throws ServiceException {
 		repositoryService.removeFile(_arg0, _arg1);
 	}
 
 	@Override
-	public void removeManufacturer(String _arg0, String _arg1) throws ServiceException {
+	public void removeManufacturer(final String _arg0, final String _arg1) throws ServiceException {
 		repositoryService.removeManufacturer(_arg0, _arg1);
 	}
 
 	@Override
-	public void removeModel(String _arg0, String _arg1) throws ServiceException {
+	public void removeModel(final String _arg0, final String _arg1) throws ServiceException {
 		repositoryService.removeModel(_arg0, _arg1);
 	}
 
 	@Override
-	public void removeOperator(String _arg0, String _arg1) throws ServiceException {
+	public void removeOperator(final String _arg0, final String _arg1) throws ServiceException {
 		repositoryService.removeOperator(_arg0, _arg1);
 	}
 
 	@Override
-	public void saveRepositoryElementMetadata(RepositoryElement _arg0) throws RepositoryException {
+	public void saveRepositoryElementMetadata(final RepositoryElement _arg0) throws RepositoryException {
 		repositoryService.saveRepositoryElementMetadata(_arg0);
 	}
 
 	@Override
-	public void updateComment(String _arg0, String _arg1) throws ServiceException {
+	public void updateComment(final String _arg0, final String _arg1) throws ServiceException {
 		repositoryService.updateComment(_arg0, _arg1);
 	}
 
 	@Override
-	public void updateFileContent(String _arg0, byte[] _arg1, String _arg2) throws ServiceException {
+	public void updateFileContent(final String _arg0, final byte[] _arg1, final String _arg2) throws ServiceException {
 		repositoryService.updateFileContent(_arg0, _arg1, _arg2);
 	}
 
 	@Override
-	public void updateFileContent(String _arg0, String _arg1, String _arg2) throws ServiceException {
+	public void updateFileContent(final String _arg0, final String _arg1, final String _arg2) throws ServiceException {
 		repositoryService.updateFileContent(_arg0, _arg1, _arg2);
 	}
 
 	@Override
-	public void updateLargeFileContent(String _arg0, byte[] _arg1, String _arg2) throws ServiceException {
+	public void updateLargeFileContent(final String _arg0, final byte[] _arg1, final String _arg2) throws ServiceException {
 		repositoryService.updateLargeFileContent(_arg0, _arg1, _arg2);
 	}
 
 	@Override
-	public void updateTag(String _arg0, String _arg1) throws ServiceException {
+	public void updateTag(final String _arg0, final String _arg1) throws ServiceException {
 		repositoryService.updateTag(_arg0, _arg1);
 	}
 
 	@Override
-	public void writePropertiesFile(String _arg0, Properties _arg1) throws ServiceException, IOException {
+	public void writePropertiesFile(final String _arg0, final Properties _arg1) throws ServiceException, IOException {
 		repositoryService.writePropertiesFile(_arg0, _arg1);
 	}
 
 	@Override
-	public StringBuffer yangFilecompilation(String _arg0) {
+	public StringBuffer yangFilecompilation(final String _arg0) {
 		return repositoryService.yangFilecompilation(_arg0);
 	}
 }
