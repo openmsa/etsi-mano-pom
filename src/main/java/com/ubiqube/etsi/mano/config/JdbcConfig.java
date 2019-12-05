144382b5996c0347d051329b8aa15cde3316c155
Olivier Vignaud <ovi@ubiqube.com>
2019-12-05 14:49:46 +0100

    Remove all H2 JDBC.

--- src/main/java/com/ubiqube/etsi/mano/config/JdbcConfig.java
+++ src/main/java/com/ubiqube/etsi/mano/config/JdbcConfig.java
@@ -6,8 +6,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 
-import com.ubiqube.etsi.mano.exception.GenericException;
-
 @Configuration
 public class JdbcConfig {
 
@@ -15,11 +13,6 @@ public class JdbcConfig {
 	public DataSource sqlDataSource(final com.ubiqube.etsi.mano.service.Configuration _conf) {
 		final DriverManagerDataSource dataSource = new DriverManagerDataSource();
 		// dataSource.setDriverClassName(_conf.build("jdbc.driver").notNull().build());
-		try {
-			Class.forName("org.h2.Driver");
-		} catch (final ClassNotFoundException e) {
-			throw new GenericException(e);
-		}
 		dataSource.setUrl(_conf.build("jdbc.url").notNull().build());
 		dataSource.setUsername(_conf.get("jdbc.username"));
 		dataSource.setPassword(_conf.get("jdbc.password"));
