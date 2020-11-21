7bced10fd2dcc4465bfb37539999ce726366e76b
Olivier Vignaud <ovi@ubiqube.com>
2020-11-21 14:53:12 +0100

    Move FrontApiTypesEnum.

--- mano-commons/src/main/java/com/ubiqube/etsi/mano/controller/FrontApiTypesEnum.java
+++ mano-commons/src/main/java/com/ubiqube/etsi/mano/controller/FrontApiTypesEnum.java
@@ -15,7 +15,7 @@
  *     along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
 
-package com.ubiqube.etsi.mano.common.v261.model;
+package com.ubiqube.etsi.mano.controller;
 
 import com.fasterxml.jackson.annotation.JsonCreator;
 
