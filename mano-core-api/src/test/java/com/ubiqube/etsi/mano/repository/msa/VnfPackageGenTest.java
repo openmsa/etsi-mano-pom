6cbe133448e75a2e4516491095628d893648352e
Olivier Vignaud <ovi@ubiqube.com>
2020-12-21 20:37:11 +0100

    More fix on TU.

--- mano-core-api/src/test/java/com/ubiqube/etsi/mano/repository/msa/VnfPackageGenTest.java
+++ mano-core-api/src/test/java/com/ubiqube/etsi/mano/repository/msa/VnfPackageGenTest.java
@@ -16,9 +16,9 @@
  */
 package com.ubiqube.etsi.mano.repository.msa;
 
-import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertTrue;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.io.IOException;
 import java.io.InputStream;
