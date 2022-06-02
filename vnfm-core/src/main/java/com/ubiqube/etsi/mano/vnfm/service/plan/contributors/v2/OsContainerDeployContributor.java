1f7da0d331fd111846ebd3671a93d1dd066be657
Olivier Vignaud <ovi@ubiqube.com>
2022-06-02 14:39:01 +0200

    vnfm-core: Remove CnfProperties.

--- vnfm-core/src/main/java/com/ubiqube/etsi/mano/vnfm/service/plan/contributors/v2/OsContainerDeployContributor.java
+++ vnfm-core/src/main/java/com/ubiqube/etsi/mano/vnfm/service/plan/contributors/v2/OsContainerDeployContributor.java
@@ -34,7 +34,6 @@ import com.ubiqube.etsi.mano.orchestrator.Bundle;
 import com.ubiqube.etsi.mano.orchestrator.nodes.Node;
 import com.ubiqube.etsi.mano.orchestrator.nodes.vnfm.OsContainerDeployableNode;
 import com.ubiqube.etsi.mano.service.VnfInstanceGatewayService;
-import com.ubiqube.etsi.mano.vnfm.config.CnfProperties;
 import com.ubiqube.etsi.mano.vnfm.jpa.VnfLiveInstanceJpa;
 import com.ubiqube.etsi.mano.vnfm.service.VnfInstanceService;
 import com.ubiqube.etsi.mano.vnfm.service.graph.VduNamingStrategy;
@@ -53,16 +52,14 @@ public class OsContainerDeployContributor extends AbstractContributorV2Base<OsCo
 	private final VnfLiveInstanceJpa vnfLiveInstanceJpa;
 	private final VduNamingStrategy vduNamingStrategy;
 	private final VnfInstanceService vnfInstanceService;
-	private final CnfProperties conf;
 
 	public OsContainerDeployContributor(final VnfInstanceGatewayService vnfInstanceGatewayService, final VnfLiveInstanceJpa vnfLiveInstanceJpa, final VduNamingStrategy vduNamingStrategy,
-			final VnfInstanceService vnfInstanceService, final CnfProperties conf) {
+			final VnfInstanceService vnfInstanceService) {
 		super();
 		this.vnfInstanceGatewayService = vnfInstanceGatewayService;
 		this.vnfLiveInstanceJpa = vnfLiveInstanceJpa;
 		this.vduNamingStrategy = vduNamingStrategy;
 		this.vnfInstanceService = vnfInstanceService;
-		this.conf = conf;
 	}
 
 	@Override
@@ -110,7 +107,6 @@ public class OsContainerDeployContributor extends AbstractContributorV2Base<OsCo
 			task.setType(ResourceTypeEnum.CNF);
 			task.setToscaName(alias);
 			task.setAlias(alias);
-			task.setTemplateId(conf.getOsTemplate());
 			ret.add(new OsContainerDeployableVt(task));
 		}
 	}
@@ -125,14 +121,8 @@ public class OsContainerDeployContributor extends AbstractContributorV2Base<OsCo
 		t.setOsContainerDeployableUnit(x);
 		t.setAlias(vduNamingStrategy.getOsContainerAlias(vnfInstance, x.getName()));
 		t.setToscaName(x.getName());
-		mapEnv(t);
 		t.setType(ResourceTypeEnum.CNF);
 		return t;
-	}
-
-	private void mapEnv(final OsContainerDeployableTask t) {
-		t.setTemplateId(conf.getOsTemplate());
-		t.setKeypair(conf.getKeypair());
 	}
 
 	private List<OsContainerDeployableVt> doTerminatePlan(final VnfInstance vnfInstance) {
