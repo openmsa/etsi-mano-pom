986e85d051814505dd8b2b61f529bca154f8a47a
Olivier Vignaud <ovi@ubiqube.com>
2022-06-15 13:58:50 +0200

    mano-core: Fix PKi service.

--- mano-core-api/src/main/java/com/ubiqube/etsi/mano/service/boot/K8sPkService.java
+++ mano-core-api/src/main/java/com/ubiqube/etsi/mano/service/boot/K8sPkService.java
@@ -17,14 +17,27 @@
 package com.ubiqube.etsi.mano.service.boot;
 
 import java.io.IOException;
+import java.io.Reader;
+import java.io.StringReader;
 import java.io.StringWriter;
 import java.io.Writer;
+import java.security.Key;
 import java.security.KeyPair;
 import java.security.KeyPairGenerator;
 import java.security.NoSuchAlgorithmException;
+import java.security.interfaces.RSAPrivateKey;
+import java.security.interfaces.RSAPublicKey;
 import java.util.Optional;
 
+import org.bouncycastle.asn1.x500.X500Name;
+import org.bouncycastle.openssl.PEMParser;
 import org.bouncycastle.openssl.jcajce.JcaMiscPEMGenerator;
+import org.bouncycastle.operator.ContentSigner;
+import org.bouncycastle.operator.OperatorCreationException;
+import org.bouncycastle.operator.jcajce.JcaContentSignerBuilder;
+import org.bouncycastle.pkcs.PKCS10CertificationRequest;
+import org.bouncycastle.pkcs.PKCS10CertificationRequestBuilder;
+import org.bouncycastle.pkcs.jcajce.JcaPKCS10CertificationRequestBuilder;
 import org.bouncycastle.util.io.pem.PemWriter;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -48,10 +61,11 @@ public class K8sPkService {
 	private static final Logger LOG = LoggerFactory.getLogger(K8sPkService.class);
 
 	private static final String K8S_PRIVATE_KEY = "k8s.private-key";
+	private static final String K8S_PUBLIC_KEY = "k8s.public-key";
 
 	private final ConfigurationsJpa configurations;
 
-	private String privateKey;
+	private KeyPair keyPair;
 
 	public K8sPkService(final ConfigurationsJpa configurations) {
 		super();
@@ -60,21 +74,55 @@ public class K8sPkService {
 	}
 
 	public void run() {
-		final Optional<Configurations> conf = configurations.findById(K8S_PRIVATE_KEY);
-		if (conf.isPresent()) {
-			LOG.info("K8s private key already exist (skipping)");
-			this.privateKey = conf.get().getWalue();
+		if (loadKeys()) {
 			return;
 		}
 		LOG.info("Creating a Private key for K8s communications.");
-		final KeyPair keyPair = generateKeyPair("RSA", 4096);
+		generateAndSaveKey();
+	}
+
+	private boolean loadKeys() {
+		final Optional<Configurations> privDbOpt = configurations.findById(K8S_PRIVATE_KEY);
+		final Optional<Configurations> pubDbOpt = configurations.findById(K8S_PUBLIC_KEY);
+		if (privDbOpt.isEmpty() || pubDbOpt.isEmpty()) {
+			return false;
+		}
+		final String privDb = privDbOpt.get().getWalue();
+		final String pubDb = pubDbOpt.get().getWalue();
+		final RSAPrivateKey priv = decodePem(privDb);
+		final RSAPublicKey pub = decodePem(pubDb);
+		keyPair = new KeyPair(pub, priv);
+		return true;
+	}
+
+	@SuppressWarnings("unchecked")
+	private static <T> T decodePem(final String pem) {
+		try (Reader reader = new StringReader(pem);
+				PEMParser pemParser = new PEMParser(reader)) {
+			return (T) pemParser.readObject();
+		} catch (final IOException e) {
+			throw new GenericException(e);
+		}
+	}
+
+	private void generateAndSaveKey() {
+		keyPair = generateKeyPair("RSA", 4096);
+		save(K8S_PRIVATE_KEY, keyPair.getPrivate());
+		save(K8S_PUBLIC_KEY, keyPair.getPublic());
+	}
+
+	private Configurations save(final String k8sKey, final Key key) {
+		final String pemKey = pemEncode(key);
+		final Configurations cf = new Configurations(k8sKey, pemKey);
+		return configurations.save(cf);
+	}
+
+	private static String pemEncode(final Object key) {
 		try (final Writer out = new StringWriter();
 				final PemWriter pw = new PemWriter(out);) {
-			pw.writeObject(new JcaMiscPEMGenerator(keyPair));
+			pw.writeObject(new JcaMiscPEMGenerator(key));
 			pw.flush();
-			final Configurations cf = new Configurations(K8S_PRIVATE_KEY, out.toString());
-			configurations.save(cf);
-			this.privateKey = cf.getWalue();
+			return out.toString();
 		} catch (final IOException e) {
 			throw new GenericException(e);
 		}
@@ -90,8 +138,18 @@ public class K8sPkService {
 		}
 	}
 
-	public String getPrivateKey() {
-		return privateKey;
+	public String createCsr(final String object) {
+		final X500Name subject = new X500Name(object);
+		final PKCS10CertificationRequestBuilder original = new JcaPKCS10CertificationRequestBuilder(subject, keyPair.getPublic());
+		final PKCS10CertificationRequestBuilder rq = new PKCS10CertificationRequestBuilder(original);
+		final JcaContentSignerBuilder csBuilder = new JcaContentSignerBuilder("SHA256withRSA");
+		ContentSigner signer;
+		try {
+			signer = csBuilder.build(keyPair.getPrivate());
+		} catch (final OperatorCreationException e) {
+			throw new GenericException(e);
+		}
+		final PKCS10CertificationRequest csr = rq.build(signer);
+		return pemEncode(csr);
 	}
-
 }
