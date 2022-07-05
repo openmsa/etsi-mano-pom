065dd25968009f1f4c1097157e36fe93eb82909a
Olivier Vignaud <ovi@ubiqube.com>
2022-07-05 16:22:27 +0200

    3.6.1: Add vnfpm Notfication endpoints.

--- etsi-models/3.6.1/model/src/main/java/com/ubiqube/etsi/mano/vnfm/v361/model/vnfpm/PerformanceInformationAvailableNotificationLinks.java
+++ etsi-models/3.6.1/model/src/main/java/com/ubiqube/etsi/mano/vnfm/v361/model/vnfpm/PerformanceInformationAvailableNotificationLinks.java
@@ -1,19 +1,3 @@
-/**
- *     Copyright (C) 2019-2020 Ubiqube.
- *
- *     This program is free software: you can redistribute it and/or modify
- *     it under the terms of the GNU General Public License as published by
- *     the Free Software Foundation, either version 3 of the License, or
- *     (at your option) any later version.
- *
- *     This program is distributed in the hope that it will be useful,
- *     but WITHOUT ANY WARRANTY; without even the implied warranty of
- *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *     GNU General Public License for more details.
- *
- *     You should have received a copy of the GNU General Public License
- *     along with this program.  If not, see <https://www.gnu.org/licenses/>.
- */
 package com.ubiqube.etsi.mano.vnfm.v361.model.vnfpm;
 
 import java.util.Objects;
@@ -24,7 +8,7 @@ import javax.validation.constraints.NotNull;
 import org.springframework.validation.annotation.Validated;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
-import com.ubiqube.etsi.mano.em.v361.model.vnflcm.NotificationLink;
+import com.ubiqube.etsi.mano.vnfm.v361.model.vnfind.NotificationLink;
 
 import io.swagger.v3.oas.annotations.media.Schema;
 
@@ -114,7 +98,7 @@ public class PerformanceInformationAvailableNotificationLinks {
 		if (this == o) {
 			return true;
 		}
-		if (o == null || getClass() != o.getClass()) {
+		if ((o == null) || (getClass() != o.getClass())) {
 			return false;
 		}
 		final PerformanceInformationAvailableNotificationLinks performanceInformationAvailableNotificationLinks = (PerformanceInformationAvailableNotificationLinks) o;
