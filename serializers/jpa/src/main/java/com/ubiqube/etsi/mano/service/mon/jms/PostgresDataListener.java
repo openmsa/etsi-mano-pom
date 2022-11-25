2aceeff7f98d7af58a631117ed9e52c88c225ed3
Olivier Vignaud <ovi@ubiqube.com>
2022-11-25 15:23:53 +0100

    Make event-api more standalone.

--- serializers/jpa/src/main/java/com/ubiqube/etsi/mano/service/mon/jms/PostgresDataListener.java
+++ serializers/jpa/src/main/java/com/ubiqube/etsi/mano/service/mon/jms/PostgresDataListener.java
@@ -29,10 +29,10 @@ import com.ubiqube.etsi.mano.dao.mano.VnfCompute;
 import com.ubiqube.etsi.mano.dao.mano.VnfInstance;
 import com.ubiqube.etsi.mano.dao.mano.pm.PmType;
 import com.ubiqube.etsi.mano.jpa.VnfInstanceJpa;
-import com.ubiqube.etsi.mano.model.NotificationEvent;
 import com.ubiqube.etsi.mano.mon.dao.AllHostMetrics;
 import com.ubiqube.etsi.mano.mon.dao.TelemetryMetricsResult;
 import com.ubiqube.etsi.mano.service.event.EventManager;
+import com.ubiqube.etsi.mano.service.event.model.NotificationEvent;
 import com.ubiqube.etsi.mano.service.mon.model.MonitoringData;
 import com.ubiqube.etsi.mano.service.mon.model.VnfIndicatorMonitoringData;
 import com.ubiqube.etsi.mano.service.mon.model.VnfIndicatorValue;
@@ -52,15 +52,14 @@ public class PostgresDataListener {
 	private final MonitoringDataJpa monitoringDataJpa;
 
 	private final VnfIndicatorValueJpa vnfIndicatorValueJpa;
-	
+
 	private final VnfIndicatorMonitoringDataJpa vnfIndicatorMonitoringDataJpa;
-	
+
 	private final EventManager eventManager;
-	
-	private VnfInstanceJpa vnfInstanceJpa;
+
+	private final VnfInstanceJpa vnfInstanceJpa;
 
 	public PostgresDataListener(final MonitoringDataJpa monitoringDataJpa, final VnfIndicatorValueJpa vnfIndicatorValueJpa, final VnfInstanceJpa vnfInstanceJpa, final VnfIndicatorMonitoringDataJpa vnfIndicatorMonitoringDataJpa, final EventManager eventManager) {
-		super();
 		this.monitoringDataJpa = monitoringDataJpa;
 		this.vnfIndicatorValueJpa = vnfIndicatorValueJpa;
 		this.vnfInstanceJpa = vnfInstanceJpa;
@@ -70,74 +69,74 @@ public class PostgresDataListener {
 
 	@JmsListener(destination = "mano.monitoring.gnocchi.data", subscription = "mano.monitoring.gnocchi.data", concurrency = "1", containerFactory = "gnocchiDataFactory")
 	public void onGnocchiData(final AllHostMetrics allHostMetrics) {
-		if (allHostMetrics == null || allHostMetrics.getTelemetryMetricsResult() == null) {
+		if ((allHostMetrics == null) || (allHostMetrics.getTelemetryMetricsResult() == null)) {
 			return;
 		}
-		if(allHostMetrics.getPmType().equals(PmType.VNF)) {
+		if (PmType.VNF.equals(allHostMetrics.getPmType())) {
 			Double averageValueByPercent = 0.0;
 			Double totalValue = 0.0;
 			final VnfInstance vnfInstance = vnfInstanceJpa.findById(allHostMetrics.getVnfInstanceId()).orElseThrow();
-			Set<VnfCompute> computes = vnfInstance.getVnfPkg().getVnfCompute();
-			if(allHostMetrics.getTelemetryMetricsResult().get(0).getKey().equals("cpu")) {
+			final Set<VnfCompute> computes = vnfInstance.getVnfPkg().getVnfCompute();
+			if ("cpu".equals(allHostMetrics.getTelemetryMetricsResult().get(0).getKey())) {
 				Long noOfVirtualCpus = 0L;
 				Boolean isMetricsUpdated = false;
-				for(VnfCompute compute : computes) {
+				for (final VnfCompute compute : computes) {
 					noOfVirtualCpus = noOfVirtualCpus + compute.getVirtualCpu().getNumVirtualCpu();
 				}
-				for(TelemetryMetricsResult action : allHostMetrics.getTelemetryMetricsResult()) {
-					LOG.info("Postgresql-Receive: {}", action);
-					VnfIndicatorMonitoringData data = vnfIndicatorMonitoringDataJpa.findByKeyAndVnfcId(allHostMetrics.getMetricName(), UUID.fromString(action.getVnfcId()));
-					if(data != null) {
-						Double deltaCpuUsage = action.getValue() - data.getValue();
-						if(deltaCpuUsage != 0.0) {
+				for (final TelemetryMetricsResult action : allHostMetrics.getTelemetryMetricsResult()) {
+					LOG.trace("Postgresql-Receive: {}", action);
+					final VnfIndicatorMonitoringData data = vnfIndicatorMonitoringDataJpa.findByKeyAndVnfcId(allHostMetrics.getMetricName(), UUID.fromString(action.getVnfcId()));
+					if (data != null) {
+						final Double deltaCpuUsage = action.getValue() - data.getValue();
+						if (deltaCpuUsage != 0.0) {
 							isMetricsUpdated = true;
 						} else {
 							isMetricsUpdated = false;
 						}
-						Double usageInSeconds = deltaCpuUsage / 1000000000L;
+						final Double usageInSeconds = deltaCpuUsage / 1000000000L;
 						totalValue = totalValue + usageInSeconds;
-					} 
-					VnfIndicatorMonitoringData data2 = new VnfIndicatorMonitoringData(allHostMetrics.getMetricName(), allHostMetrics.getMasterJobId(), action.getValue(), UUID.fromString(action.getVnfcId()));
+					}
+					final VnfIndicatorMonitoringData data2 = new VnfIndicatorMonitoringData(allHostMetrics.getMetricName(), allHostMetrics.getMasterJobId(), action.getValue(), UUID.fromString(action.getVnfcId()));
 					vnfIndicatorMonitoringDataJpa.save(data2);
 				}
-				if(!isMetricsUpdated) {
+				if (!isMetricsUpdated) {
 					return;
 				}
 				averageValueByPercent = (totalValue / (noOfVirtualCpus * 600)) * 100;
-			} else if(allHostMetrics.getTelemetryMetricsResult().get(0).getKey().equals("memory.usage")) {
+			} else if ("memory.usage".equals(allHostMetrics.getTelemetryMetricsResult().get(0).getKey())) {
 				Long totalMemorySize = 0L;
-				for(VnfCompute compute : computes) {
+				for (final VnfCompute compute : computes) {
 					totalMemorySize = totalMemorySize + compute.getVirtualMemory().getVirtualMemSize();
 				}
-				for(TelemetryMetricsResult action : allHostMetrics.getTelemetryMetricsResult()) {
-					LOG.info("Postgresql-Receive: {}", action);
-					Double bytesValue = action.getValue() * 1000000;
+				for (final TelemetryMetricsResult action : allHostMetrics.getTelemetryMetricsResult()) {
+					LOG.trace("Postgresql-Receive: {}", action);
+					final Double bytesValue = action.getValue() * 1000000;
 					totalValue = totalValue + bytesValue;
 				}
-				Double averageValue = totalValue / allHostMetrics.getTelemetryMetricsResult().size();
-				if(totalMemorySize != 0) {
+				final Double averageValue = totalValue / allHostMetrics.getTelemetryMetricsResult().size();
+				if (totalMemorySize != 0) {
 					averageValueByPercent = (averageValue / totalMemorySize) * 100;
 				}
 			} else {
 				averageValueByPercent = 0.0;
 			}
-			
-			VnfIndicatorValue existingVnfIndicatorValue = vnfIndicatorValueJpa.findByKeyAndVnfInstanceId(allHostMetrics.getMetricName(), allHostMetrics.getVnfInstanceId());
-			if(existingVnfIndicatorValue != null && !existingVnfIndicatorValue.getValue().equals(averageValueByPercent)) {
-					//vnf indicator value change notification should be sent.
-				LOG.info("{} indicator value changed to {}", allHostMetrics.getVnfInstanceId()+":"+allHostMetrics.getMetricName(), averageValueByPercent);
-				
+
+			final VnfIndicatorValue existingVnfIndicatorValue = vnfIndicatorValueJpa.findByKeyAndVnfInstanceId(allHostMetrics.getMetricName(), allHostMetrics.getVnfInstanceId());
+			if ((existingVnfIndicatorValue != null) && !existingVnfIndicatorValue.getValue().equals(averageValueByPercent)) {
+				// vnf indicator value change notification should be sent.
+				LOG.info("{} indicator value changed to {}", allHostMetrics.getVnfInstanceId() + ":" + allHostMetrics.getMetricName(), averageValueByPercent);
+
 				eventManager.sendNotification(NotificationEvent.VNF_INDICATOR_VALUE_CHANGED, allHostMetrics.getVnfInstanceId(), Map.of("vnfIndicatorId", allHostMetrics.getMetricName(), "value", String.valueOf(averageValueByPercent), "vnfInstanceId", allHostMetrics.getVnfInstanceId().toString(), "vnfdId", vnfInstance.getVnfdId()));
 			}
 			final VnfIndicatorValue vnfIndValue = new VnfIndicatorValue(allHostMetrics.getMetricName(), allHostMetrics.getMasterJobId(), averageValueByPercent, allHostMetrics.getVnfInstanceId());
 			vnfIndicatorValueJpa.save(vnfIndValue);
 			return;
 		}
-		for(TelemetryMetricsResult action : allHostMetrics.getTelemetryMetricsResult()) {
+		for (final TelemetryMetricsResult action : allHostMetrics.getTelemetryMetricsResult()) {
 			LOG.info("Postgresql-Receive: {}", action);
 			final MonitoringData entity = new MonitoringData(action.getKey(), action.getMasterJobId(), action.getTimestamp(), action.getValue(), action.getVnfcId(), action.isStatus());
 			monitoringDataJpa.save(entity);
 		}
 	}
-	
+
 }
