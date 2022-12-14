ec830ca660e32630dbaf8d6b9f55dd2692e97100
Olivier Vignaud <ovi@ubiqube.com>
2022-12-14 17:22:29 +0100

    Minor code cleanup.

--- nfvo-core/src/main/java/com/ubiqube/etsi/mano/nfvo/service/rest/NfvoVnfInstanceLcm.java
+++ nfvo-core/src/main/java/com/ubiqube/etsi/mano/nfvo/service/rest/NfvoVnfInstanceLcm.java
@@ -29,7 +29,6 @@ import com.ubiqube.etsi.mano.controller.vnflcm.VnfInstanceLcm;
 import com.ubiqube.etsi.mano.dao.mano.CancelModeTypeEnum;
 import com.ubiqube.etsi.mano.dao.mano.VnfInstance;
 import com.ubiqube.etsi.mano.dao.mano.config.Servers;
-import com.ubiqube.etsi.mano.dao.mano.dto.VnfLcmOpOccs;
 import com.ubiqube.etsi.mano.dao.mano.v2.VnfBlueprint;
 import com.ubiqube.etsi.mano.dao.mano.vnfi.ChangeExtVnfConnRequest;
 import com.ubiqube.etsi.mano.model.VnfHealRequest;
@@ -104,7 +103,7 @@ public class NfvoVnfInstanceLcm implements VnfInstanceLcm {
 				.vnfInstance(vnfInstanceId)
 				.scale(scaleVnfRequest);
 	}
-	
+
 	@Override
 	public VnfBlueprint heal(final Servers servers, final UUID vnfInstanceId, final VnfHealRequest vnfHealRequest) {
 		return manoClientFactory.getClient(servers)
