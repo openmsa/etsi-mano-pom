4eb9aa798e7677b1c52ef15634dcc1e3746cbee4
Olivier Vignaud <ovi@ubiqube.com>
2023-02-21 10:54:11 +0100

    mano-em: Add quick client.

---
+++ mano-em/em-client/src/main/java/com/ubiqube/etsi/mano/em/client/config/ClientConfigBean.java
@@ -0,0 +1,66 @@
+/**
+ *     Copyright (C) 2019-2020 Ubiqube.
+ *
+ *     This program is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     This program is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public License
+ *     along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+package com.ubiqube.etsi.mano.em.client.config;
+
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.client.DefaultClientRequestObservationConvention;
+import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.reactive.function.client.WebClient.Builder;
+import org.springframework.web.reactive.function.client.support.WebClientAdapter;
+import org.springframework.web.service.invoker.HttpServiceProxyFactory;
+
+import com.ubiqube.etsi.mano.em.client.vnfind.VnfIndRemoteService;
+import com.ubiqube.etsi.mano.em.client.vnfind.VnfIndSubscriptionRemoteService;
+
+import io.micrometer.observation.ObservationRegistry;
+
+@Component
+public class ClientConfigBean {
+
+	private final DefaultClientRequestObservationConvention oc;
+	private final ObservationRegistry observationRegistry;
+
+	public ClientConfigBean(final ConfigurableApplicationContext configurableApplicationContext) {
+		oc = new DefaultClientRequestObservationConvention("http.client.requests");
+		observationRegistry = configurableApplicationContext.getBean(ObservationRegistry.class);
+	}
+
+	@Bean
+	VnfIndRemoteService vnfIndRemoteService() {
+		final HttpServiceProxyFactory proxyFactory = createProxyFactory();
+		return proxyFactory.createClient(VnfIndRemoteService.class);
+	}
+
+	@Bean
+	VnfIndSubscriptionRemoteService vnfIndSubscriptionRemoteService() {
+		final HttpServiceProxyFactory proxyFactory = createProxyFactory();
+		return proxyFactory.createClient(VnfIndSubscriptionRemoteService.class);
+	}
+
+	private HttpServiceProxyFactory createProxyFactory() {
+		final Builder webBuilder = WebClient.builder()
+				.baseUrl("http://localhost:8110/sol002/");
+		webBuilder.defaultHeader("Version", "1.10.0");
+		webBuilder.observationConvention(oc);
+		webBuilder.observationRegistry(observationRegistry);
+		final WebClient client = webBuilder
+				.build();
+		return HttpServiceProxyFactory.builder(WebClientAdapter.forClient(client)).build();
+	}
+}
