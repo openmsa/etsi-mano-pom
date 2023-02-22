d0cbcf31e65e90cc484ee85d7114a9a7be3f342a
Olivier Vignaud <ovi@ubiqube.com>
2023-02-22 14:11:57 +0100

    monitoring: Rename spring boot runner class.

--- montoring/mano-mon-webapp/src/main/java/com/ubiqube/etsi/mano/MonitoringApplication.java
+++ montoring/mano-mon-webapp/src/main/java/com/ubiqube/etsi/mano/MonitoringApplication.java
@@ -14,25 +14,10 @@
  *     You should have received a copy of the GNU General Public License
  *     along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
-/**
- * This copy of Woodstox XML processor is licensed under the
- * Apache (Software) License, version 2.0 ("the License").
- * See the License for details about distribution rights, and the
- * specific rights regarding derivate works.
- *
- * You may obtain a copy of the License at:
- *
- * http://www.apache.org/licenses/
- *
- * A copy is also included in the downloadable source code package
- * containing Woodstox, in file "ASL2.0", under the same directory
- * as this file.
- */
 package com.ubiqube.etsi.mano;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.jms.annotation.EnableJms;
 import org.springframework.scheduling.annotation.EnableScheduling;
@@ -41,10 +26,10 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @EnableScheduling
 @EnableJms
 @ComponentScan("com.ubiqube.etsi.mano")
-public class Application extends SpringBootServletInitializer {
+public class MonitoringApplication {
 
 	public static void main(final String[] args) {
-		SpringApplication.run(Application.class, args);
+		SpringApplication.run(MonitoringApplication.class, args);
 	}
 
 }
