767e3920b1ddbb3bd3b744874da3631c24067bcd
Olivier Vignaud <ovi@ubiqube.com>
2023-02-22 16:05:37 +0100

    monitoring: Add client webapp.

---
+++ mano-mon-webapp-client/src/main/java/com/ubiqube/etsi/mano/service/mon/cli/service/MonitoringRemoteService.java
@@ -0,0 +1,58 @@
+/**
+ *     Copyright (C) 2019-2020 Ubiqube.
+ *
+ *     This program is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     This program is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public License
+ *     along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+package com.ubiqube.etsi.mano.service.mon.cli.service;
+
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.function.client.DefaultClientRequestObservationConvention;
+import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.reactive.function.client.WebClient.Builder;
+import org.springframework.web.reactive.function.client.support.WebClientAdapter;
+import org.springframework.web.service.invoker.HttpServiceProxyFactory;
+
+import com.ubiqube.etsi.mano.service.mon.cli.MonPollingRemoteService;
+
+import io.micrometer.observation.ObservationRegistry;
+
+@Configuration(proxyBeanMethods = false)
+public class MonitoringRemoteService {
+	private final DefaultClientRequestObservationConvention oc;
+	private final ObservationRegistry observationRegistry;
+
+	public MonitoringRemoteService(final ConfigurableApplicationContext configurableApplicationContext) {
+		oc = new DefaultClientRequestObservationConvention("http.client.requests");
+		observationRegistry = configurableApplicationContext.getBean(ObservationRegistry.class);
+	}
+
+	@Bean
+	MonPollingRemoteService createMonPollingRemoteService() {
+		final HttpServiceProxyFactory proxyFactory = createProxyFactory();
+		return proxyFactory.createClient(MonPollingRemoteService.class);
+	}
+
+	private HttpServiceProxyFactory createProxyFactory() {
+		final Builder webBuilder = WebClient.builder()
+				.baseUrl("http://mano-mon:8080/");
+		webBuilder.observationConvention(oc);
+		webBuilder.observationRegistry(observationRegistry);
+		final WebClient client = webBuilder
+				.build();
+		return HttpServiceProxyFactory.builder(WebClientAdapter.forClient(client)).build();
+	}
+
+}
