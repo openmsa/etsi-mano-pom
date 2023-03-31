0355817cbff2bb4ef87870fb95c39bffb6c52c63
Olivier Vignaud <ovi@ubiqube.com>
2023-03-31 17:44:32 +0200

    monitoring: Add test unit.

---
+++ mano-mon-core/src/test/java/com/ubiqube/etsi/mano/mon/core/config/JmsConfigurationTest.java
@@ -0,0 +1,48 @@
+/**
+ *     Copyright (C) 2019-2023 Ubiqube.
+ *
+ *     This program is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     This program is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public License
+ *     along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+package com.ubiqube.etsi.mano.mon.core.config;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.boot.autoconfigure.jms.DefaultJmsListenerContainerFactoryConfigurer;
+import org.springframework.jms.support.converter.MessageConverter;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import jakarta.jms.ConnectionFactory;
+
+class JmsConfigurationTest {
+
+	@SuppressWarnings("static-method")
+	@Test
+	void test() {
+		final JmsConfiguration jc = new JmsConfiguration();
+		final MessageConverter mc = Mockito.mock(MessageConverter.class);
+		final ConnectionFactory connFactory = Mockito.mock(ConnectionFactory.class);
+		jc.expirityQueueJmsTemplate(connFactory, mc);
+		final ObjectMapper mapper = Mockito.mock(ObjectMapper.class);
+		jc.jacksonJmsMessageConverter(mapper);
+		final DefaultJmsListenerContainerFactoryConfigurer configurer = Mockito.mock(DefaultJmsListenerContainerFactoryConfigurer.class);
+		jc.serialzeDataFactory(connFactory, configurer);
+		jc.queueJmsTemplate(connFactory, mc);
+		jc.topicJmsTemplate(connFactory, mc);
+		assertTrue(true);
+	}
+
+}
---
+++ mano-mon-core/src/test/java/com/ubiqube/etsi/mano/mon/core/service/PollingServiceTest.java
@@ -0,0 +1,56 @@
+/**
+ *     Copyright (C) 2019-2023 Ubiqube.
+ *
+ *     This program is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     This program is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public License
+ *     along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+package com.ubiqube.etsi.mano.mon.core.service;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import com.ubiqube.etsi.mano.service.mon.data.BatchPollingJob;
+
+@ExtendWith(MockitoExtension.class)
+class PollingServiceTest {
+	@Mock
+	private PollingJobService pollingService;
+	@Mock
+	private BusHandlerService busService;
+
+	@Test
+	void testEmpty() {
+		final PollingService ps = new PollingService(pollingService, busService);
+		when(pollingService.findRunnableJobs(any())).thenReturn(List.of());
+		ps.run();
+		assertTrue(true);
+	}
+
+	@Test
+	void test() {
+		final PollingService ps = new PollingService(pollingService, busService);
+		final BatchPollingJob batchpollingjob = new BatchPollingJob();
+		when(pollingService.findRunnableJobs(any())).thenReturn(List.of(batchpollingjob));
+		ps.run();
+		assertTrue(true);
+	}
+
+}
