753a114e0b7266a3f8734ecbeb33b10687c4b75c
Olivier Vignaud <ovi@ubiqube.com>
2023-04-26 11:19:51 +0200

    vnfm-core: Add test units.

---
+++ vnfm-core/src/test/java/com/ubiqube/etsi/mano/vnfm/service/metrics/MetricsServiceTest.java
@@ -0,0 +1,36 @@
+/**
+ *     Copyright (C) 2019-2023 Ubiqube.
+ *
+ *     This program is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     This program is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public License
+ *     along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+package com.ubiqube.etsi.mano.vnfm.service.metrics;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+import io.micrometer.core.instrument.MeterRegistry;
+import io.micrometer.core.instrument.simple.SimpleMeterRegistry;
+
+class MetricsServiceTest {
+
+	@Test
+	void test() {
+		final MeterRegistry reg = new SimpleMeterRegistry();
+		final MetricsService srv = new MetricsService(reg);
+		srv.getVnfmOnboardingCounter();
+		assertTrue(true);
+	}
+
+}
