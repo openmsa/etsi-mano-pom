2760e826618109e0659e0c5b2d01dbbd4385bb70
Olivier Vignaud <ovi@ubiqube.com>
2023-05-04 11:27:45 +0200

    Add monitoring tests.

---
+++ montoring/mano-mon-webapp-client/src/test/java/com/ubiqube/etsi/mano/service/mon/cli/service/MonitoringRemoteObservabilityServiceTest.java
@@ -0,0 +1,65 @@
+/**
+ *     Copyright (C) 2019-2023 Ubiqube.
+ *
+ *     This program is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     This program is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public License
+ *     along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+package com.ubiqube.etsi.mano.service.mon.cli.service;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.when;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.context.ConfigurableApplicationContext;
+
+import com.ubiqube.etsi.mano.service.mon.cli.MonitoringProperty;
+
+import io.micrometer.observation.ObservationRegistry;
+
+@ExtendWith(MockitoExtension.class)
+class MonitoringRemoteObservabilityServiceTest {
+	@Mock
+	private ConfigurableApplicationContext configurableApp;
+	@Mock
+	private ObservationRegistry obsReg;
+
+	@Test
+	void testCreateMetricsRemoteService() {
+		final MonitoringProperty props = new MonitoringProperty();
+		when(configurableApp.getBean(ObservationRegistry.class)).thenReturn(obsReg);
+		final MonitoringRemoteObservabilityService srv = new MonitoringRemoteObservabilityService(configurableApp, props);
+		srv.createMetricsRemoteService();
+		assertTrue(true);
+	}
+
+	@Test
+	void testCreateMonPollingRemoteService() {
+		final MonitoringProperty props = new MonitoringProperty();
+		when(configurableApp.getBean(ObservationRegistry.class)).thenReturn(obsReg);
+		final MonitoringRemoteObservabilityService srv = new MonitoringRemoteObservabilityService(configurableApp, props);
+		srv.createMonPollingRemoteService();
+		assertTrue(true);
+	}
+
+	@Test
+	void testCreateProxyFactory() {
+		final MonitoringProperty props = new MonitoringProperty();
+		when(configurableApp.getBean(ObservationRegistry.class)).thenReturn(obsReg);
+		final MonitoringRemoteObservabilityService srv = new MonitoringRemoteObservabilityService(configurableApp, props);
+		srv.createProxyFactory();
+		assertTrue(true);
+	}
+}
