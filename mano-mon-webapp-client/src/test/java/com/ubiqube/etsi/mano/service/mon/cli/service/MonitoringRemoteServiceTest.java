52b8301c58f7bfa7f90567cb790fe7ee90adf0f1
Olivier Vignaud <ovi@ubiqube.com>
2023-05-04 11:27:45 +0200

    Add monitoring tests.

---
+++ mano-mon-webapp-client/src/test/java/com/ubiqube/etsi/mano/service/mon/cli/service/MonitoringRemoteServiceTest.java
@@ -0,0 +1,51 @@
+/**
+ *     Copyright (C) 2019-2023 Ubiqube.
+ *
+ *     This program is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     This program is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public License
+ *     along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+package com.ubiqube.etsi.mano.service.mon.cli.service;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+import com.ubiqube.etsi.mano.service.mon.cli.MonitoringProperty;
+
+@SuppressWarnings("static-method")
+class MonitoringRemoteServiceTest {
+
+	@Test
+	void testCreateMetricsRemoteService() {
+		final MonitoringProperty props = new MonitoringProperty();
+		final MonitoringRemoteService srv = new MonitoringRemoteService(props);
+		srv.createMetricsRemoteService();
+		assertTrue(true);
+	}
+
+	@Test
+	void testCreateMonPollingRemoteService() {
+		final MonitoringProperty props = new MonitoringProperty();
+		final MonitoringRemoteService srv = new MonitoringRemoteService(props);
+		srv.createMonPollingRemoteService();
+		assertTrue(true);
+	}
+
+	@Test
+	void testCreateProxyFactory() {
+		final MonitoringProperty props = new MonitoringProperty();
+		final MonitoringRemoteService srv = new MonitoringRemoteService(props);
+		srv.createProxyFactory();
+		assertTrue(true);
+	}
+}
