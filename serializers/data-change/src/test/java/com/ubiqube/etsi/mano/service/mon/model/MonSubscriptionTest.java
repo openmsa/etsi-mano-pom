598420fdb56a82d198e6ca68c78e06b9a4134f8b
Olivier Vignaud <ovi@ubiqube.com>
2023-06-10 16:58:17 +0200

    mon-data-change: Add test units.

---
+++ serializers/data-change/src/test/java/com/ubiqube/etsi/mano/service/mon/model/MonSubscriptionTest.java
@@ -0,0 +1,55 @@
+/**
+ *     Copyright (C) 2019-2023 Ubiqube.
+ *
+ *     This program is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     This program is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public License
+ *     along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+package com.ubiqube.etsi.mano.service.mon.model;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+import nl.jqno.equalsverifier.EqualsVerifier;
+import nl.jqno.equalsverifier.EqualsVerifierReport;
+import nl.jqno.equalsverifier.Warning;
+
+class MonSubscriptionTest {
+
+	@Test
+	void test() {
+		final MonSubscription srv = new MonSubscription();
+		srv.setAuthParamOauth2(null);
+		srv.setCallBackUrl(null);
+		srv.setId(null);
+		srv.setKey(null);
+		srv.getAuthParamOauth2();
+		srv.getCallBackUrl();
+		srv.getId();
+		srv.getKey();
+		srv.equals(srv);
+		srv.equals(null);
+		srv.equals("");
+		assertTrue(true);
+	}
+
+	@Test
+	void testEqual() {
+		final EqualsVerifierReport rep = EqualsVerifier
+				.simple()
+				.forClass(MonSubscription.class)
+				.suppress(Warning.INHERITED_DIRECTLY_FROM_OBJECT, Warning.SURROGATE_KEY)
+				.report();
+		System.out.println("" + rep.getMessage());
+	}
+}
