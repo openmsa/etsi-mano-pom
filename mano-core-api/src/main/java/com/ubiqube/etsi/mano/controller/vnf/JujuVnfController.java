3bfaad4c00c5bdb5236d923bbc0a368333c82552
Olivier Vignaud <ovi@ubiqube.com>
2023-08-22 11:24:31 +0200

    Remove juju controller @NotNull annotations.

--- mano-core-api/src/main/java/com/ubiqube/etsi/mano/controller/vnf/JujuVnfController.java
+++ mano-core-api/src/main/java/com/ubiqube/etsi/mano/controller/vnf/JujuVnfController.java
@@ -8,10 +8,9 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.ubiqube.etsi.mano.exception.GenericException;
 import com.ubiqube.etsi.mano.service.juju.cli.JujuRemoteService;
 import com.ubiqube.etsi.mano.service.juju.entities.JujuCloud;
-
-import jakarta.validation.constraints.NotNull;
 
 @RestController
 @RequestMapping("/juju")
@@ -25,41 +24,41 @@ public class JujuVnfController {
 	}
 
 	@PostMapping(value = "/instantiate")
-	public ResponseEntity<String> instantiate(@RequestBody @NotNull final JujuCloud jCloud) throws Exception {
+	public ResponseEntity<String> instantiate(@RequestBody final JujuCloud jCloud) {
 		LOG.info("Juju instantiating...");
 		if (!remoteService.addCloud(jCloud).getStatusCode().is2xxSuccessful()) {
-			throw new Exception("Error Create Cloud");
+			throw new GenericException("Error Create Cloud");
 		}
 		if (!remoteService.addCredential(jCloud).getStatusCode().is2xxSuccessful()) {
-			throw new Exception("Error Create Credential");
+			throw new GenericException("Error Create Credential");
 		}
 //		if (!remoteService.genMetadata("path", "imageId", "osSeries", "region", "osAuthUrl").getStatusCode().is2xxSuccessful()) throw new Exception("Error Create Metadata"); //No need as will be there
 		if (!remoteService.addController(jCloud.getName(), jCloud.getMetadata()).getStatusCode().is2xxSuccessful()) {
-			throw new Exception("Error Create Controller");
+			throw new GenericException("Error Create Controller");
 		}
 		if (!remoteService.addModel(jCloud.getMetadata().getModels().get(0).getName()).getStatusCode().is2xxSuccessful()) {
-			throw new Exception("Error Create Model");
+			throw new GenericException("Error Create Model");
 		}
 		if (!remoteService.deployApp(jCloud.getMetadata().getModels().get(0).getCharmName(), jCloud.getMetadata().getModels().get(0).getAppName()).getStatusCode().is2xxSuccessful()) {
-			throw new Exception("Error Deploying Charm");
+			throw new GenericException("Error Deploying Charm");
 		}
 
 		return ResponseEntity.ok("Success");
 	}
 
 	@PostMapping(value = "/terminate")
-	public ResponseEntity<String> terminate(@RequestBody @NotNull final JujuCloud jCloud) throws Exception {
+	public ResponseEntity<String> terminate(@RequestBody final JujuCloud jCloud) {
 		LOG.info("Juju terminating...");
 //		if (!remoteService.removeApplication("charm").getStatusCode().is2xxSuccessful()) throw new Exception("Error Removing Charm"); //No needed as will be removed by Controller
 //		if (!remoteService.removeModel("modelName").getStatusCode().is2xxSuccessful()) throw new Exception("Error Removing Model"); //No needed as will be removed by Controller
 		if (!remoteService.removeController(jCloud.getMetadata().getName()).getStatusCode().is2xxSuccessful()) {
-			throw new Exception("Error Removing Controller");
+			throw new GenericException("Error Removing Controller");
 		}
 		if (!remoteService.removeCredential(jCloud.getName(), jCloud.getCredential().getName()).getStatusCode().is2xxSuccessful()) {
-			throw new Exception("Error Removing Credential");
+			throw new GenericException("Error Removing Credential");
 		}
 		if (!remoteService.removeCloud(jCloud.getName()).getStatusCode().is2xxSuccessful()) {
-			throw new Exception("Error Removing Cloud");
+			throw new GenericException("Error Removing Cloud");
 		}
 		return ResponseEntity.ok("Success");
 	}
