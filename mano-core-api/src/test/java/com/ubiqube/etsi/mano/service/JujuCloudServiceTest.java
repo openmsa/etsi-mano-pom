ae9bcc4f0cfd00070976d26edb9fcd51374bcdcf
yhz108 <yhz@ubiqube.com>
2023-12-07 12:23:55 +0800

    Test cases for instantiate and terminate

--- mano-core-api/src/test/java/com/ubiqube/etsi/mano/service/JujuCloudServiceTest.java
+++ mano-core-api/src/test/java/com/ubiqube/etsi/mano/service/JujuCloudServiceTest.java
@@ -17,23 +17,36 @@
 package com.ubiqube.etsi.mano.service;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 
+import com.ubiqube.etsi.mano.exception.VnfmException;
 import com.ubiqube.etsi.mano.jpa.JujuCloudJpa;
 import com.ubiqube.etsi.mano.jpa.JujuCredentialJpa;
 import com.ubiqube.etsi.mano.jpa.JujuMetadataJpa;
 import com.ubiqube.etsi.mano.service.juju.cli.JujuRemoteService;
 import com.ubiqube.etsi.mano.service.juju.entities.JujuCloud;
+import com.ubiqube.etsi.mano.service.juju.entities.JujuCredential;
+import com.ubiqube.etsi.mano.service.juju.entities.JujuMetadata;
+import com.ubiqube.etsi.mano.service.juju.entities.JujuModel;
 
 @ExtendWith(MockitoExtension.class)
 public class JujuCloudServiceTest {
@@ -48,13 +61,13 @@ public class JujuCloudServiceTest {
 	private JujuMetadataJpa jujuMetadataJpa;
 
 	@Mock
-	private JujuRemoteService jujuRemoteService;
+	private JujuRemoteService remoteService;
 	
 	private JujuCloudService jujuCloudService;
 
 	@BeforeEach
 	void init() {
-		jujuCloudService = new JujuCloudService(jujuCloudJpa, jujuCredentialJpa, jujuMetadataJpa, jujuRemoteService);
+		jujuCloudService = new JujuCloudService(jujuCloudJpa, jujuCredentialJpa, jujuMetadataJpa, remoteService);
 	}
 
 	@Test
@@ -72,4 +85,87 @@ public class JujuCloudServiceTest {
 		when(jujuCloudJpa.findByMetadataName(controllerName,"PASS")).thenReturn(expectedResult);
 		assertEquals(1, jujuCloudService.findByMetadataName(controllerName,"PASS").size());
 	}
+	
+	@Test
+	public void testJujuInstantiate() throws Exception {
+		JujuMetadata jujuMetadata = new JujuMetadata();
+		JujuCredential jujuCredential = new JujuCredential();
+		JujuModel model = new JujuModel("test", "test", "test");
+		List<JujuModel> models = new ArrayList<>();
+		models.add(model);
+		jujuCredential.setName("test");
+		jujuMetadata.setName("test");
+		jujuMetadata.setModels(models);
+		JujuCloud jCloud = new JujuCloud();
+		jCloud.setName("test");
+		jCloud.setCredential(jujuCredential);
+		jCloud.setMetadata(jujuMetadata);
+		Mockito.when(jujuCloudJpa.findById(any())).thenReturn(Optional.of(jCloud));
+		ResponseEntity<String> responseobject = new ResponseEntity<String>("test", HttpStatus.OK);
+		System.out.println(responseobject.getBody());
+		Mockito.when(remoteService.controllerDetail(jCloud.getMetadata().getName())).thenReturn(responseobject);
+		boolean result = jujuCloudService.jujuInstantiate(UUID.randomUUID());
+		assertTrue(result);
+	}
+
+	@Test
+	public void testJujuInstantiateThrowsException() throws Exception {
+	    final UUID id = UUID.randomUUID();
+		Exception exception = assertThrows(VnfmException.class, () -> {
+			jujuCloudService.jujuInstantiate(id);
+		});
+		String expectedMsg = "Could not find Juju Cloud";
+		String actualMsg = exception.getMessage();
+		assertTrue(actualMsg.contains(expectedMsg));
+	}
+
+	@Test
+	public void testJujuInstantiateThrowsExceptionWhenCloudnameNull() throws Exception {
+		final UUID id = UUID.randomUUID();
+		JujuCloud jCloud = new JujuCloud();
+		Mockito.when(jujuCloudJpa.findById(any())).thenReturn(Optional.of(jCloud));
+        boolean result = jujuCloudService.jujuInstantiate(id);
+        assertFalse(result);
+
+	}
+
+	@Test
+	public void testJujuInstantiateThrowsExceptionWhenNoControllerFound() throws Exception {
+		final UUID id = UUID.randomUUID();
+		JujuMetadata jujuMetadata = new JujuMetadata();
+		JujuCredential jujuCredential = new JujuCredential();
+		jujuCredential.setName("test");
+		jujuMetadata.setName("test");
+		JujuCloud jCloud = new JujuCloud();
+		jCloud.setName("test");
+		jCloud.setCredential(jujuCredential);
+		jCloud.setMetadata(jujuMetadata);
+		Mockito.when(jujuCloudJpa.findById(id)).thenReturn(Optional.of(jCloud));
+		ResponseEntity<String> responseobject = new ResponseEntity<String>("ERROR", HttpStatus.NOT_FOUND);
+		Mockito.when(remoteService.controllerDetail(Mockito.anyString())).thenReturn(responseobject);
+		boolean result = jujuCloudService.jujuInstantiate(id);
+        assertFalse(result);
+	}
+	
+	@Test
+	public void testJujuterminate() throws Exception {
+		final UUID id = UUID.randomUUID();
+		JujuMetadata jujuMetadata = new JujuMetadata();
+		jujuMetadata.setName("test");
+		JujuCloud jCloud = new JujuCloud();
+		jCloud.setMetadata(jujuMetadata);
+		Mockito.when(jujuCloudJpa.findById(id)).thenReturn(Optional.of(jCloud));
+		boolean result = jujuCloudService.jujuTerminate(id);
+		assertTrue(result);
+	}
+
+	@Test
+	public void testJujuterminateThrowsVnfmException() throws Exception {
+		final UUID id = UUID.randomUUID();
+        Mockito.when(jujuCloudJpa.findById(id)).thenReturn(Optional.empty());
+        boolean result = jujuCloudService.jujuTerminate(id);
+        assertFalse(result);
+	}
+
+
 }
