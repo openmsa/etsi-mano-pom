e55fa9ed6345b9e5b16d2c96a4613648cfb1d665
Olivier Vignaud <ovi@ubiqube.com>
2024-03-05 15:13:18 +0100

    Update license headers.

--- vnfm-webapp/src/main/java/com/ubiqube/etsi/mano/VnfmApplication.java
+++ vnfm-webapp/src/main/java/com/ubiqube/etsi/mano/VnfmApplication.java
@@ -1,5 +1,5 @@
 /**
- *     Copyright (C) 2019-2023 Ubiqube.
+ *     Copyright (C) 2019-2024 Ubiqube.
  *
  *     This program is free software: you can redistribute it and/or modify
  *     it under the terms of the GNU General Public License as published by
@@ -14,7 +14,6 @@
  *     You should have received a copy of the GNU General Public License
  *     along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
-
 package com.ubiqube.etsi.mano;
 
 import java.security.Security;
