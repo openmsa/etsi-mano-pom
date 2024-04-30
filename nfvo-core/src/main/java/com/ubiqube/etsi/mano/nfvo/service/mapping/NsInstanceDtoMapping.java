9447e5c7614088500e1cd315ba2d39615a1cf7d1
Olivier Vignaud <ovi@ubiqube.com>
2024-04-30 17:17:50 +0200

    🚨 nfvo-core: Fix some mapper warnings.

--- nfvo-core/src/main/java/com/ubiqube/etsi/mano/nfvo/service/mapping/NsInstanceDtoMapping.java
+++ nfvo-core/src/main/java/com/ubiqube/etsi/mano/nfvo/service/mapping/NsInstanceDtoMapping.java
@@ -24,10 +24,16 @@ import org.mapstruct.MappingTarget;
 import com.ubiqube.etsi.mano.dao.mano.NsLiveInstance;
 import com.ubiqube.etsi.mano.dao.mano.NsdInstance;
 import com.ubiqube.etsi.mano.dao.mano.NsdPackage;
+import com.ubiqube.etsi.mano.dao.mano.ScaleInfo;
 import com.ubiqube.etsi.mano.dao.mano.VnfInstance;
+import com.ubiqube.etsi.mano.dao.mano.VnfScaleInfo;
 import com.ubiqube.etsi.mano.dao.mano.alarm.ResourceHandle;
 import com.ubiqube.etsi.mano.dao.mano.dto.nsi.NsInstanceDto;
 import com.ubiqube.etsi.mano.dao.mano.dto.nsi.VnfInstanceDto;
+import com.ubiqube.etsi.mano.dao.mano.dto.nsi.VnfInstanceInstantiatedVnfInfoDto;
+import com.ubiqube.etsi.mano.dao.mano.nfvo.NsVnfInstance;
+import com.ubiqube.etsi.mano.dao.mano.nslcm.scale.NsScaleInfo;
+import com.ubiqube.etsi.mano.dao.mano.v2.BlueprintParameters;
 
 @Mapper(componentModel = MappingConstants.ComponentModel.SPRING)
 public interface NsInstanceDtoMapping {
@@ -49,9 +55,37 @@ public interface NsInstanceDtoMapping {
 	@Mapping(target = "virtualLinkInfo", ignore = true)
 	void map(NsdInstance ret, @MappingTarget NsInstanceDto dto);
 
+	@Mapping(target = "extensions", ignore = true)
+	@Mapping(target = "instantiatedVnfInfo", ignore = true)
+	@Mapping(target = "instantiationState", ignore = true)
+	@Mapping(target = "metadata", ignore = true)
+	@Mapping(target = "vimId", ignore = true)
+	@Mapping(target = "vnfConfigurableProperties", ignore = true)
+	@Mapping(target = "vnfInstanceDescription", ignore = true)
+	@Mapping(target = "vnfInstanceName", ignore = true)
+	@Mapping(target = "vnfPkgId", ignore = true)
+	@Mapping(target = "vnfProductName", ignore = true)
+	@Mapping(target = "vnfProvider", ignore = true)
+	@Mapping(target = "vnfSoftwareVersion", ignore = true)
+	@Mapping(target = "vnfdId", ignore = true)
+	@Mapping(target = "vnfdVersion", ignore = true)
+	VnfInstanceDto map(NsVnfInstance o);
+
+	@Mapping(target = "aspectId", ignore = true)
+	@Mapping(target = "scaleLevel", ignore = true)
+	ScaleInfo map(NsScaleInfo o);
+
 	@Mapping(target = "vimId", ignore = true)
 	@Mapping(target = "vnfPkgId", source = "vnfPkg.id")
 	VnfInstanceDto map(VnfInstance x);
+
+	@Mapping(target = "monitoringParameters", ignore = true)
+	@Mapping(target = "vnfState", ignore = true)
+	VnfInstanceInstantiatedVnfInfoDto map(BlueprintParameters o);
+
+	@Mapping(target = "numberOfSteps", ignore = true)
+	@Mapping(target = "scaleType", ignore = true)
+	VnfScaleInfo map(ScaleInfo o);
 
 	@Mapping(target = "containerNamespace", ignore = true)
 	@Mapping(target = "vimLevelAdditionalResourceInfo", ignore = true)
