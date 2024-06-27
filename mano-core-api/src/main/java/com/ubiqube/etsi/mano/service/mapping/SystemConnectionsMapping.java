59d764175f1b8dc6d87963d49fe4f3d6bd98a45f
Olivier Vignaud <ovi@ubiqube.com>
2024-06-27 17:50:46 +0200

    🐛 core: Fix system connection mappings.

--- mano-core-api/src/main/java/com/ubiqube/etsi/mano/service/mapping/SystemConnectionsMapping.java
+++ mano-core-api/src/main/java/com/ubiqube/etsi/mano/service/mapping/SystemConnectionsMapping.java
@@ -19,6 +19,8 @@ package com.ubiqube.etsi.mano.service.mapping;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.MappingConstants;
+import org.mapstruct.MappingTarget;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import com.ubiqube.etsi.mano.dao.mano.AccessInfo;
 import com.ubiqube.etsi.mano.dao.mano.InterfaceInfo;
@@ -28,11 +30,36 @@ import com.ubiqube.etsi.mano.orchestrator.entities.SystemConnections;
 @Mapper(componentModel = MappingConstants.ComponentModel.SPRING)
 public interface SystemConnectionsMapping {
 	@Mapping(target = "moduleName", ignore = true)
+	@Mapping(target = "interfaceInfo", ignore = true)
+	@Mapping(target = "accessInfo", ignore = true)
 	SystemConnections map(VimConnectionInformation o);
 
-	@Mapping(target = "id", ignore = true)
-	AccessInfo map(AccessInfo o);
+	@PostMapping
+	default void map(VimConnectionInformation o, @MappingTarget SystemConnections sc) {
+		if (null == o)
+			return;
+		if ("ETSINFV.OPENSTACK_KEYSTONE.V_3".equals(o.getVimType())) {
+			sc.setInterfaceInfo(mapToOpenstackV3InterfaceInfo(o.getInterfaceInfo()));
+			sc.setAccessInfo(mapToOpenstackV3Ai(o.getAccessInfo()));
+
+		} else if ("UBINFV.CISM.V_1".equals(o.getVimType())) {
+			sc.setInterfaceInfo(mapToK8sInterfaceInfo(o.getInterfaceInfo()));
+			sc.setAccessInfo(mapToK8sAi(o.getAccessInfo()));
+		} else if ("PAAS".equals(o.getVimType())) {
+			// XXX: This need works works.
+		}
+	}
 
 	@Mapping(target = "id", ignore = true)
-	InterfaceInfo map(InterfaceInfo o);
+	AccessInfo mapToOpenstackV3Ai(AccessInfo accessInfo);
+
+	@Mapping(target = "id", ignore = true)
+	InterfaceInfo mapToOpenstackV3InterfaceInfo(InterfaceInfo interfaceInfo);
+
+	@Mapping(target = "id", ignore = true)
+	AccessInfo mapToK8sAi(AccessInfo accessInfo);
+
+	@Mapping(target = "id", ignore = true)
+	InterfaceInfo mapToK8sInterfaceInfo(InterfaceInfo interfaceInfo);
+
 }
